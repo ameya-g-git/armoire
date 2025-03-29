3365344c3172d6a9436d9419c166c4656bc6b545
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-03-29 12:36:26 -0400

    added funky checkbox

--- dashboard/index.php
+++ dashboard/index.php
@@ -8,11 +8,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Dashboard</title>
 </head>
 
 <body>
-
+    <div id="form">
+        <form action="post.php" method="post">
+            <fieldset>
+                <legend for="title">title</legend>
+                <input type="text" name="title" id="title">
+            </fieldset>
+            <fieldset>
+                <legend for="content">post content</legend>
+                <textarea rows="5" id="content" name="content"></textarea>
+            </fieldset>
+            <label for="market">
+                <input type="checkbox" name="market" id="market">
+                list on marketplace?
+            </label>
+            <div id="marketplace-input">
+                <fieldset>
+                    <legend for="price">price</legend>
+                    <input type="number" step="0.01" min="1" name="price" id="price">
+                </fieldset>
+                <fieldset>
+                    <legend for="stock">stock</legend>
+                    <input type="number" step="1" min="1" max="100" name="stock" id="stock">
+                </fieldset>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
